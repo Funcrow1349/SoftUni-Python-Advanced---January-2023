5b39957ee6e820a32e00eab912aa040fb0c3ae4f
Funcrow1349 <113984399+Funcrow1349@users.noreply.github.com>
2023-01-19 21:54:59 +0200

    Stacks and Queues - homework

--- exercise_3_stacks_queues_tuples_and_sets/06_paint_colors.py
+++ exercise_3_stacks_queues_tuples_and_sets/06_paint_colors.py
@@ -1,44 +1,57 @@
+#   You will have to find all possible color combinations that can be used.
+#   Write a program that finds colors in a string. You will be given a string on a single line containing substrings
+#   (separated by a single space) from which you will be able to form the following colors:
+#   Main colors: "red", "yellow", "blue"
+#   Secondary colors: "orange", "purple", "green"
+#   To form a color, you should concatenate the first and the last substrings and check if you can get any of the above
+#   colors' names. If there is only one substring left, you should use it to do the same check.
+#   You can only keep a secondary color if the two main colors needed for its creation could be formed from the given
+#   substrings:
+#   •	orange = red + yellow
+#   •	purple = red + blue
+#   •	green = yellow + blue
+#       Note: You could find main colors needed to keep a secondary color after it is found.
+#   When you form a color, remove both substrings. Otherwise, you should remove the last character of each substring and
+#   return them in the middle of the original string. If the string contains an odd number of substrings, you should put
+#   the substrings one position ahead.
+#   For example, if you are given the string "re yellow bye" you could not form a color with the substring "re" and
+#   "bye",so you should remove the last character and return them in the middle of the string: "r by yellow".
+#   In the end, print out the list with colors in the order in which they are found.
+#   Input
+#   •	Single line string
+#   Output
+#   •	The list with the collected colors
+#   Constrains
+#   •	You will not receive an empty string
+#   •	Please consider only the colors mentioned above
+#   •	There won't be any cases with repeating colors
+
 from collections import deque
 
 initial_string = deque(input().split())
-colors = []
+colors = {"red", "blue", "yellow", "orange", "purple", "green"}
+combinations = {
+    "orange": {"red", "yellow"},
+    "purple": {"red", "blue"},
+    "green": {"blue", "yellow"},
+}
 
-while len(initial_string) >= 2:
-    first_segment = initial_string.popleft()
-    second_segment = initial_string.pop()
-    full_segment = first_segment + second_segment
-    full_segment_two = second_segment + first_segment
-    if full_segment in ["red", "yellow", "blue", "orange", "purple", "green"]:
-        colors.append(full_segment)
-    elif full_segment_two in ["red", "yellow", "blue", "orange", "purple", "green"]:
-        colors.append(full_segment_two)
-    else:
-        first_segment = first_segment[:-1]
-        second_segment = second_segment[:-1]
-        middle_of_the_string = len(initial_string) // 2
-        if first_segment:
-            initial_string.insert(middle_of_the_string - 1, first_segment)
-        if second_segment:
-            initial_string.insert(middle_of_the_string, second_segment)
+found_colors = []
 
-if initial_string:
-    while initial_string[0] != "":
-        if initial_string[0] in ["red", "yellow", "blue", "orange", "purple", "green"]:
-            colors.append(initial_string[0])
-            initial_string.pop()
+while initial_string:
+    first_part = initial_string.popleft()
+    second_part = initial_string.pop() if initial_string else ""
+    for combination in (first_part + second_part, second_part + first_part):
+        if combination in colors:
+            found_colors.append(combination)
             break
-        else:
-            initial_string[0] = initial_string[0][:-1]
+    else:
+        for element in (first_part[:-1], second_part[:-1]):
+            if element:
+                initial_string.insert(len(initial_string) // 2, element)
 
-for color in colors:
-    if color == "orange":
-        if "yellow" not in colors or "red" not in colors:
-            colors.remove(color)
-    elif color == "purple":
-        if "blue" not in colors or "red" not in colors:
-            colors.remove(color)
-    elif color == "green":
-        if "yellow" not in colors or "blue" not in colors:
-            colors.remove(color)
+for color in set(combinations.keys()).intersection(found_colors):
+    if not combinations[color].issubset(found_colors):
+        found_colors.remove(color)
 
-print(colors)
+print(found_colors)
