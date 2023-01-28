9fb0b590b111f497d107a57552f383a25f0bced8
Funcrow1349 <113984399+Funcrow1349@users.noreply.github.com>
2023-01-28 15:47:40 +0200

    Error handling - homework

---
+++ lecture_5_error_handling/02_repeat_text.py
@@ -0,0 +1,15 @@
+#  Write a program that receives a text on the first line and times (to repeat the text) that must be an integer. If the
+#  user passes a non-integer type for the times variable, handle the exception and print a message "Variable times must
+#  be an integer".
+
+def repeat_text(text, times):
+    return text * times
+
+
+try:
+    text_to_repeat = input()
+    number = int(input())
+    print(repeat_text(text_to_repeat, number))
+
+except ValueError:
+    print("Variable times must be an integer")
