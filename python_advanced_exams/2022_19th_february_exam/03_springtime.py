fb48da03c4b50983c0b380e4bb21d930e9d4feb5
Ivaylo Ivanov <113984399+Funcrow1349@users.noreply.github.com>
2023-02-06 09:08:25 +0100

    test exam

---
+++ python_advanced_exams/2022_19th_february_exam/03_springtime.py
@@ -0,0 +1,68 @@
+#   Spring is the season of new beginnings. Fresh buds bloom, animals awaken and the earth seems to come to life again.
+#   Farmers and gardeners plant their seeds and temperatures slowly rise.
+#   Write a function called start_spring which will receive a different number of keyword arguments.
+#   Each keyword holds a key with a name of the spring object (string), and each value holds its type (string). For
+#   example, dahlia="flower", shrikes="bird", dogwood="tree".
+#   The function should sort the given spring objects in collections by their type:
+#   •	The collections sorted by their number of elements in descending order. If two or more collections have the same
+#       number of elements in them, return them in ascending order (alphabetically) by the type's name.
+#   •	Each collection's elements should be sorted in ascending order (alphabetically) by the object's name.
+#   Input
+#   •	There will be no input. Just parameters passed to your function.
+#   Output
+#   •	Return the result, sorted as described above in the format:
+#       o	"{type_one}:
+#           -{spring_object_of_this_type_one}
+#           -{spring_object_of_this_type_two}
+#               …
+#           -{spring_object_of_this_type_N}
+#           {type_two}:
+#               …
+#           {type_N}:
+#               …
+#           -{last_spring_object_of_typeN}"
+
+def start_spring(**spring_data):
+    types_of_objects = {}
+    final_result = ""
+
+    for key, value in spring_data.items():
+        if value not in types_of_objects.keys():
+            types_of_objects[value] = []
+        types_of_objects[value].append(key)
+
+    types_of_objects = dict(sorted(types_of_objects.items(), key=lambda x: (-len(x[1]), x[0])))
+
+    for key, values in types_of_objects.items():
+        final_result += f"{key}:\n"
+        for value in sorted(values):
+            final_result += f"-{value}\n"
+
+    return final_result
+
+
+example_objects = {"Water Lilly": "flower",
+                   "Swifts": "bird",
+                   "Callery Pear": "tree",
+                   "Swallows": "bird",
+                   "Dahlia": "flower",
+                   "Tulip": "flower",}
+print(start_spring(**example_objects))
+
+# example_objects = {"Swallow": "bird",
+#                    "Thrushes": "bird",
+#                    "Woodpeckers": "bird",
+#                    "Swallows": "bird",
+#                    "Warblers": "bird",
+#                    "Shrikes": "bird",}
+# print(start_spring(**example_objects))
+
+# example_objects = {"Magnolia": "tree",
+#                    "Swallow": "bird",
+#                    "Thrushes": "bird",
+#                    "Pear": "tree",
+#                    "Cherries": "tree",
+#                    "Shrikes": "bird",
+#                    "Butterfly": "insect"}
+# print(start_spring(**example_objects))
+
